060588bf344d73448bc2fbd79a92f69548c276a2
nucamisoo <nucamisoo@gmail.com>
2022-04-10 00:05:56 +0900

    Get URL of the active tab

--- popup.js
+++ popup.js
@@ -1,5 +1,8 @@
-document.getElementById("btn-on").addEventListener("click", function() { 
-    alert('ON');
+document.getElementById("btn-on").addEventListener("click", function() {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        const url = tabs[0].url;
+        alert('ON pressed in the tab:' + url);
+    });
 });
 
 document.getElementById("btn-off").addEventListener('click', function () {
