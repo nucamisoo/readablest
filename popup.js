944a80eda4c8c4aafe3dfa78c4a951b9cc96b75f
nucamisoo <nucamisoo@gmail.com>
2022-04-09 23:36:52 +0900

    Alerts run when button is clicked

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.getElementById("btn-on").addEventListener("click", function() { 
+    alert('ON');
+});
+
+document.getElementById("btn-off").addEventListener('click', function () {
+    alert('OFF');
+});
