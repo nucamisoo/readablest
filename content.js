0534d3764226e0e7ee698c5f727a44d054794e36
nucamisoo <nucamisoo@gmail.com>
2022-04-11 22:03:01 +0900

    Press on and the browser reads out the text

--- content.js
+++ content.js
@@ -3,6 +3,10 @@ chrome.runtime.onMessage.addListener(
         if (request.msg === "ON") {
             alert("Got ON message in content.js");
             document.body.style.background = "blue";
+            alert(document.getElementById("text").innerText);
+            const uttr = new SpeechSynthesisUtterance(document.getElementById("text").innerText);
+            uttr.lang = "en-US"
+            speechSynthesis.speak(uttr);
         } else {
             alert("Something went wrong.");
         }
