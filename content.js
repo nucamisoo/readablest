fe4aa0ec8dbc2acc0c2734ccbba914bdc37972c1
nucamisoo <nucamisoo@gmail.com>
2022-04-17 11:59:49 +0900

    Click to read out the text

--- content.js
+++ content.js
@@ -1,12 +1,25 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.msg === "ON") {
-            alert("Got ON message in content.js");
-            document.body.style.background = "blue";
-            alert(document.getElementById("text").innerText);
-            const uttr = new SpeechSynthesisUtterance(document.getElementById("text").innerText);
-            uttr.lang = "en-US"
-            speechSynthesis.speak(uttr);
+            var allTags = document.getElementsByTagName('*');
+
+            for (var i = 0; i < allTags.length; i++) {
+                allTags.item(i).style.border = "solid 1px blue"
+            }
+
+            window.addEventListener("click", function(e) {
+                
+                //e.preventDefault();
+
+                var x = e.clientX;
+                var y = e.clientY;
+                var z = document.elementFromPoint(x, y);
+
+                const uttr = new SpeechSynthesisUtterance(z.textContent);
+                uttr.lang = "en-US"
+                speechSynthesis.speak(uttr);
+            });
+
         } else {
             alert("Something went wrong.");
         }
--- popup.js
+++ popup.js
@@ -2,7 +2,6 @@ document.getElementById("btn-on").addEventListener("click", function() {
 
     chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
         const url = tabs[0].url;
-        alert('ON pressed in the tab:' + url);
 
         chrome.tabs.sendMessage(
             tabs[0].id,
